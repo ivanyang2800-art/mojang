88964aaf36b8bddf19e97938e96d00477906119b
RyonTanka <ryontanka@gmail.com>
2025-11-25 20:37:06 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,22 @@
 import mysql.connector as dbsql
 
-class database():
+class Database:
     def __init__(self):
-        self.mydb=dbsql.connect(
+        self.mydb = dbsql.connect(
             host="localhost",
             user="root",
             password="",
             database="ecommerce"
-        )
+            )
         self.cursor = self.mydb.cursor()
+        def get_data(self, query):
+            self.cursor.execute(query)
+            return self.cursor.fetchall()
 
-db= database()
-print(db.mydb)
+
+        def process_data_query(self, query):
+            self.cursor.execute(query)
+            self.mydb.commit()
+            return True
+        
+db_connection = Database()
